50b006afada300926eca1760490c48104cc382f2
VYushinsky <78491821+VYushinsky@users.noreply.github.com>
2021-02-06 21:00:51 +0300

    Add files via upload

---
+++ src/Point.java
@@ -0,0 +1,12 @@
+public class Point {
+    double x, y;
+
+    Point(double px, double py) {
+        this.x = px;
+        this.y = py;
+    }
+
+    @Override public String toString() {
+        return this.x + " " + this.y;
+    }
+}
